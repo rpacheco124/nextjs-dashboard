47232b782e8a9403ac930fd697d0e9fc8a69c4b8
Ronald Pacheco <rpacheco124@gmail.com>
2025-01-09 19:31:41 +0100

    Chapter 09 completed

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,5 @@
-export default function InvoicesPage() {
+export default async function InvoicesPage() {
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
     return <h1>Invoices page</h1>
 }
